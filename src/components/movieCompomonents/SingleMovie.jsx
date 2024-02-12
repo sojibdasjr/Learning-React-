d8804f1bea4c3b9b3b8be791c86b0ccce63e5194
sojibdasjr <contact.sojibdas@gmail.com>
2024-02-13 01:07:21 +0600

    flex css added

--- src/components/movieCompomonents/SingleMovie.jsx
+++ src/components/movieCompomonents/SingleMovie.jsx
@@ -3,13 +3,17 @@ import './Movie.css'
 
 const SingleMovie = ({movieProps}) => {
     console.log(movieProps);
-    const {movieName, releaseDate, actor} = movieProps
+    const {movieName, releaseDate, actor, imageLink} = movieProps
     
     return (
         <div className="movie_box">
+            <img src={imageLink} alt="" />
+            <div>
             <h3>Movie Name: {movieName} </h3>
             <h4>ReleaseDate: {releaseDate} </h4>
             <h5>Actor : {actor}</h5>
+            </div>
+            
         </div>
     );
 };
